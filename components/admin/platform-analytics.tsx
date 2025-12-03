a2687fce7530f3b6fbe22cda79f0982462a011b2
Gospiece <danielgospiece@gmail.com>
2025-12-03 06:41:43 +0100

    Delete components directory

--- components/admin/platform-analytics.tsx
+++
@@ -1,8 +0,0 @@
-export function PlatformAnalytics() {
-  return (
-    <div className="bg-blue-50 border border-blue-200 rounded-lg p-6">
-      <h3 className="text-lg font-semibold text-blue-800 mb-4">Platform Analytics</h3>
-      <p className="text-blue-700">Analytics dashboard coming soon!</p>
-    </div>
-  )
-}
